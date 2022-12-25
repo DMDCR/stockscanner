a961367af49653d3785aead438395dbbbfcfeebb
DMDIcefox <111153358+DMDCR@users.noreply.github.com>
2022-12-25 12:31:09 -0600

    Add files via upload
    stuff

---
+++ main.py
@@ -0,0 +1,47 @@
+import requests
+import pandas as pd
+import smtplib
+from email.mime.text import MIMEText
+
+# Email credentials
+EMAIL_ADDRESS = 'email_address'
+EMAIL_PASSWORD = 'password'
+RECIPIENT_EMAIL_ADDRESS = 'sendingto'
+
+# Function to get real-time stock data from Yahoo Finance
+def get_stock_data(symbol):
+  url = f'https://query1.finance.yahoo.com/v7/finance/quote?symbols={symbol}'
+  response = requests.get(url)
+  data = response.json()
+  return data['quoteResponse']['result'][0]
+
+# Licensing
+
+# Code made by dmdcr on Github
+# @dmdev_ on youtube!
+
+# DO NOT REMOVE THIS
+
+#             |
+#             |
+#rest of code v
+
+# Function to check if stock meets trade criteria
+def check_trade_criteria(stock_data):
+  # Insert code to check if stock meets trade criteria here
+  pass
+
+# Scan the stock market for trades
+symbols = ['AAPL', 'GOOG', 'MSFT']  # List of symbols to scan
+for symbol in symbols:
+  stock_data = get_stock_data(symbol)
+  if check_trade_criteria(stock_data):
+    # Send email notification for potential trade
+    message = f'Potential trade identified for {symbol}'
+    msg = MIMEText(message)
+    msg['Subject'] = 'Potential Trade Identified'
+    msg['From'] = EMAIL_ADDRESS
+    msg['To'] = RECIPIENT_EMAIL_ADDRESS
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+      smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+      smtp.send_message(msg)
